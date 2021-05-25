b197d9f0280986ed61344b1d57d1e583de82d039
Bharadwaja-rao-D <80561036+Bharadwaja-rao-D@users.noreply.github.com>
2021-05-25 11:20:34 +0530

    updated headerfile

--- opengl.h
+++ opengl.h
@@ -4,6 +4,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <stack>
+#include <string>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
@@ -25,9 +27,84 @@ class shader_t{
     static unsigned get_shader_program_id();
 };
 
+
+
+class data_t{
+    public:
+    //only this changes for its derived classes
+    data_t()=default;
+	float vertices[24*6] = {
+
+        //back face red color
+
+        -0.5f, -0.5f, -0.5f,  1.0f, 0.0f,0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 0.0f,0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 0.0f,0.0f,
+        
+        //front face green color 
+
+        -0.5f, -0.5f,  0.5f,   0.0f, 1.0f,0.0f,
+         0.5f, -0.5f,  0.5f,   0.0f, 1.0f,0.0f,
+         0.5f,  0.5f,  0.5f,   0.0f, 1.0f,0.0f,
+        -0.5f,  0.5f,  0.5f,   0.0f, 1.0f,0.0f,
+        
+        
+        //left face blue color
+
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 0.0f,1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,1.0f,
+        
+        
+        //right face yellow color
+
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 1.0f,0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 1.0f,0.0f,
+       
+        
+        //down face
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,1.0f,
+        0.5f, -0.5f, -0.5f,   0.0f, 1.0f,1.0f,
+        0.5f, -0.5f,  0.5f,   0.0f, 1.0f,1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 1.0f,1.0f,
+        
+        
+        //top face
+
+        -0.5f,  0.5f, -0.5f,  1.0f, 0.0f,1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 0.0f,1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,1.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,1.0f
+        
+
+
+
+    } ;
+
+unsigned indices[36]={   
+     0, 1, 2, 2, 3, 0,
+    4, 5, 6, 6, 7, 4,
+    8, 9, 10, 10, 11, 8,
+    12, 13, 14, 14, 15, 12,
+    16, 17, 18, 18, 19, 16,
+    20, 21, 22, 22, 23, 20};
+GLsizei indices_size();
+/**
+ * 
+ * It loads (saves) the data in to the buffer that is binded 
+ *   
+*/
 void init_buffer();
 
-GLsizei indices_size ();
+
+};
+
+
 
 //call back functions
 
@@ -44,9 +121,23 @@ static glm::mat4 transform_matrix ;
 public:
 static void pass_transform_matrix();
 static void scroll_callback(GLFWwindow *window, double xoffset , double yoffset);
-static void rotate_callback(GLFWwindow *window, unsigned normal_key, int modifier_key);
+static void key_callback(GLFWwindow *window, unsigned normal_key, int modifier_key);
+
 };
 
 
 
+//we should have only one history maintainer and one set ofmovement matrix
+//hence it is better to use singleton here
+class movement_t{
+private:
+	static movement_t* Instance;
+	static std::stack<unsigned>history;
+	movement_t()=delete;
+public:
+	//need to add the actual movement matrices here 
+	static movement_t* getInstance();
+	static void undo_option();
+	//static void move_callback(GLFWwindow *window,unsigned unicode);
+};
 #endif
