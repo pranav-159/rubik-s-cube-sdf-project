6ae31129bd98184be1c9701ea3d8b3238c9c141a
rajasekhar156 <80587161+rajasekhar156@users.noreply.github.com>
2021-07-23 12:15:02 +0530

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -7,41 +7,34 @@ Menu::Menu(float width ,float height)
      //error
    }
 
-   if(!texture.loadFromFile("rubix-cube_image.jpg"))
+   if(!texture.loadFromFile("Rubik's-cube.jpg"))
    {
      //error
    }
    
    rect.setTexture( &texture );
-   rect.setSize(sf::Vector2f(1920,1000));  
+   rect.setSize(sf::Vector2f(1900,900));  
    rect.setPosition(sf::Vector2f(0,0));
 
-   rect1.setPosition(sf::Vector2f(70* width / 100 , height /(10) *5));
-   rect1.setSize(sf::Vector2f(100,100));
+   rect1.setPosition(sf::Vector2f(70* width / 100 , height /(10) *4));
+   rect1.setSize(sf::Vector2f(250,150));
    rect1.setFillColor(sf::Color::Transparent);
    
    rect2.setPosition(sf::Vector2f(70* width / 100 , height /(10) *7));
-   rect2.setSize(sf::Vector2f(100,100));
+   rect2.setSize(sf::Vector2f(200,150));
    rect2.setFillColor(sf::Color::Transparent);
   
    menu[0].setFont( font );
    menu[0].setFillColor(sf::Color::Black);
-   menu[0].setCharacterSize(100);
+   menu[0].setCharacterSize(150);
    menu[0].setString("Play");
-   menu[0].setPosition(sf::Vector2f(70* width / 100 , height /(10) *5));
+   menu[0].setPosition(sf::Vector2f(70* width / 100 , height /(10) *4));
 
    menu[1].setFont( font );
    menu[1].setFillColor(sf::Color::Black);
-   menu[1].setCharacterSize(100);
+   menu[1].setCharacterSize(150);
    menu[1].setString("Exit");
    menu[1].setPosition(sf::Vector2f(70* width / 100 , height /(10) *7));
-
-   menu[2].setFont( font );
-   menu[2].setFillColor(sf::Color::Black );
-   menu[2].setCharacterSize(200);
-   menu[2].setString("RUBIX-CUBE GAME");
-   menu[2].setPosition(sf::Vector2f(10* width / 100 , height /(10) *(1/2)));
-  
 }
 
 sf::RectangleShape Menu::getrect1()
